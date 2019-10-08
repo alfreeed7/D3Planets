92c7424f939aa705fccf2b669488af488c265182
Alfred Zhong <alfred.zhong@centre.edu>
2019-10-08 15:44:18 -0400

    hi

--- planets.js
+++ planets.js
@@ -1,3 +1,33 @@
-
-
+var names=["Swag","Yolo","Lol"]
+var A1=function()
+{
+d3.select("#A1")
+  .append("p")
+  .text(names)}
+A1()
  
+var A2=function()
+{
+d3.select("#A2")
+  .selectAll("span")
+  .data(names)
+  .enter()
+  .append("span")
+  .text(function(d){return d})
+}
+A2()
+
+var B1 = d3.json("planets.json")
+B1.then(function(wut)
+       {
+    console.log("wut", wut)
+    
+    
+},
+  function(err)
+{
+    console.log("fail",err;)
+    
+})
+
+B1()
